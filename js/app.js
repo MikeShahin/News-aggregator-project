aa88eaf776d18e59003f34a0ffe89e3ff55627fb
MikeShah <mikeshah@mail.sfsu.edu>
2020-04-27 21:28:51 -0700

    first update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 //////////////////////////////////////////
-// Add all Javascript code to this file.
+// Add all Javascript code to this file.    
 /////////////////////////////////////////
+
+const headers = document.querySelectorAll('h3');
+const guardianKey = key
+fetch(`https://content.guardianapis.com/search?q=debates&api-key=${guardianKey}`) 
+.then(response => {
+    console.log(response);
+    return response.json();
+})
+.then(data => {
+    console.log(data)
+    console.log(data.response.results[2].webTitle);
+    const newsResult = data.response.results;
+    for(let i = 0; i < headers.length; i++) {
+        let header = headers[i];
+        let article = newsResult[i];
+        let title = article.webTitle;
+        header.textContent = title; 
+    }
+  })
